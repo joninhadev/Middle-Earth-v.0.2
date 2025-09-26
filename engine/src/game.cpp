9c4661e3c42ca64b620ff770f5599ba72cf200a0
joninhadev <joninhac10@gmail.com>
2025-09-25 23:58:28 -0300

    added: functions (setMovementBlocked, setMonsterSpeed)

--- engine/src/game.cpp
+++ engine/src/game.cpp
@@ -789,6 +789,11 @@ void Game::playerMoveCreature(Player* player, Creature* movingCreature, const Po
 		return;
 	}
 
+	if (movingCreature->isMovementBlocked()) {
+		player->sendCancelMessage(RETURNVALUE_NOTMOVEABLE);
+		return;
+	}
+
 	player->setNextActionTask(nullptr);
 
 	if (!Position::areInRange<1, 1, 0>(movingCreatureOrigPos, player->getPosition())) {
@@ -2273,6 +2278,11 @@ void Game::playerMove(uint32_t playerId, Direction direction)
 {
 	Player* player = getPlayerByID(playerId);
 	if (!player) {
+		return;
+	}
+
+	if (player->isMovementBlocked()) {
+		player->sendCancelWalk();
 		return;
 	}
 
@@ -5579,6 +5589,18 @@ void Game::changeSpeed(Creature* creature, int32_t varSpeedDelta)
 	varSpeed += varSpeedDelta;
 
 	creature->setSpeed(varSpeed);
+
+	//send to clients
+	SpectatorHashSet spectators;
+	map.getSpectators(spectators, creature->getPosition(), false, true);
+	for (Creature* spectator : spectators) {
+		spectator->getPlayer()->sendChangeSpeed(creature, creature->getStepSpeed());
+	}
+}
+
+void Game::setCreatureSpeed(Creature* creature, int32_t speed)
+{
+	creature->setBaseSpeed(speed);
 
 	//send to clients
 	SpectatorHashSet spectators;
